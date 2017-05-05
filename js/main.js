aa076b81daa00aff3f7a9629816d479c06d63b76
John Liu <john831118@gmail.com>
2017-05-05 11:52:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCLJl3kDjpnnO8F41n9j9rG_gdOjY_Abi8",
+  authDomain: "simpsonaccounter.firebaseapp.com",
+  databaseURL: "https://simpsonaccounter.firebaseio.com",
+  projectId: "simpsonaccounter",
+  storageBucket: "simpsonaccounter.appspot.com",
+  messagingSenderId: "472259175988"
+};
+firebase.initializeApp(config);
+
+
 var fdb = new ForerunnerDB();
 var db = fdb.db("資料庫");
 MoneyCollection = db.collection('moneys');
